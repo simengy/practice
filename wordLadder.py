ca75b49275eb0e32efc6447eababae02f5b07b55
syan-aa <syan@appannie.com>
2017-10-04 22:07:02 -0700

    added BFS word Ladder

--- wordLadder.py
+++ wordLadder.py
@@ -1,8 +1,8 @@
 def wordLadder(word, last, dictionary, label):
-    
+
     if search(word, last):
         return True
-    
+
     N = len(dictionary)
     for tag in range(N):
 
@@ -10,10 +10,10 @@ def wordLadder(word, last, dictionary, label):
             continue
         else:
             print tag, word, dictionary[tag]
-            
+
             if search(word, dictionary[tag]):
                 label[tag] = 1
-                
+
                 if wordLadder(dictionary[tag], last, dictionary, label):
                     return True
                 else:
@@ -27,23 +27,52 @@ def wordLadder(word, last, dictionary, label):
 
     return False
 
+
 def search(word, tag):
 
     count = 0
     for i in range(len(word)):
         if word[i] == tag[i]:
             count += 1
-    
+
     if count == len(word) - 1:
         return True
     else:
         return False
 
 
-dictionary = ['hot', 'dot', 'dog', 'lot', 'log' ]
+def wordLadder_bfs(start, end, dictionary):
+    q = []
+
+    dictionary.add(end)
+    q.append((start, 1))
+
+    while q:
+        word, cnt = q.pop(0)
+        if word == end:
+            return cnt
+
+        for ind in xrange(len(word)):
+            part1 = word[:ind]
+            part2 = word[ind + 1:]
+
+            for l in 'abcdefghijklmnopqrstuvwxyz':
+                if word[ind] != l:
+                    new_word = part1 + l + part2
+                    if new_word in dictionary:
+                        q.append((new_word, cnt + 1))
+                        dictionary.remove(new_word)
+
+    return None
+
+
+dictionary = ['hot', 'dot', 'dog', 'lot', 'log']
 # indicator to represent whether the words have been touched or not
-label = [ 0 for i in range(len(dictionary))]
+label = [0 for i in range(len(dictionary))]
 word = 'hit'
 last = 'cog'
 print label
 print wordLadder(word, last, dictionary, label)
+
+
+print wordLadder_bfs(word, last, set(dictionary))
