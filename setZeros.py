0eea5e4921a89baf05b08964b94118ba8987374a
syan-aa <syan@appannie.com>
2017-10-10 10:30:21 -0700

    setZeros and sortColor

--- setZeros.py
+++ setZeros.py
@@ -1,34 +1,34 @@
 def setZeros(matrix):
 
-    pos = []
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
+    M = len(matrix)
+    N = len(matrix[0])
+
+    rowZeros = set()
+    colZeros = set()
+
+    for i in range(M):
+        for j in range(N):
 
             if matrix[i][j] == 0:
-                temp = [i,j] 
+                rowZeros.add(i)
+                colZeros.add(j)
 
-                pos.append(temp)
+    for i in rowZeros:
+        for j in range(N):
+            matrix[i][j] = 0
 
-    put(matrix, pos)
-
-
-def put(matrix, pos):
-
-    for i in pos:
-        row = i[0]
-        col = i[1]
-        for c in range(len(matrix[row])): 
-            matrix[row][c] = 0
-
-        for r in range(len(matrix)):
-            matrix[r][col] = 0
+    for j in colZeros:
+        for i in range(M):
+            matrix[i][j] = 0
 
 
-matrix = [[1,2,3,4],[2,3,0,5],[0,3,2,5],[1,1,1,1]]
+matrix = [[1, 2, 3, 4], [2, 3, 0, 5], [0, 3, 2, 5], [1, 1, 1, 1]]
 
+print 'Raw matrix:'
 for i in matrix:
     print i
 setZeros(matrix)
 
+print 'Set Zero matrix:'
 for i in matrix:
     print i
--- sortColor.py
+++ sortColor.py
@@ -1,5 +1,8 @@
+# Sort an array with 0,1,2
+# Example: [0,2,1,0,2,1] --> [0,0,1,1,2,2]
+
+
 def sortColor(arrary):
-    
     N = len(arrary)
     A = [1] * N
     ind1 = 0
@@ -12,13 +15,35 @@ def sortColor(arrary):
         elif i == 2:
             A[ind2] = 2
             ind2 -= 1
-        
+
     return A
 
+# in-place
 
-array = [0,1,2,1,2,1,2,0,1,1]
+
+def sortColor_v2(array):
+    N = len(array)
+    ind1 = 0
+    ind2 = N - 1
+    i = 0
+
+    while i <= ind2:
+        if array[i] == 0:
+            array[ind1], array[i] = array[i], array[ind1]
+            ind1 += 1
+            i += 1
+        elif array[i] == 2:
+            array[ind2], array[i] = array[i], array[ind2]
+            ind2 -= 1
+        else:
+            i += 1
+
+
+array = [0, 1, 2, 1, 2, 1, 2, 0, 1, 1]
 
 
 print array
 print sortColor(array)
-        
+
+sortColor_v2(array)
+print array
